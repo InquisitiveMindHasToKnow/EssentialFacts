af3b88243cfc603c1761961ccf74f5dc921a2b87
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-02-25 00:39:00 -0500

    Added audio files for some study questions. Currently a random file is chosen on creation of studywithaudio fragment

--- app/src/main/java/org/ohmstheresistance/essentialfacts/fragments/StudyWithAudioFragment.kt
+++ app/src/main/java/org/ohmstheresistance/essentialfacts/fragments/StudyWithAudioFragment.kt
@@ -1,6 +1,5 @@
 package org.ohmstheresistance.essentialfacts.fragments
 
-
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
@@ -12,6 +11,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.Runnable
 import org.ohmstheresistance.essentialfacts.R
 import org.ohmstheresistance.essentialfacts.databinding.StudyWithAudioFragmentBinding
@@ -28,7 +28,14 @@ class StudyWithAudioFragment : Fragment() {
     lateinit var runnable: Runnable
     lateinit var currentTimeTextView: TextView
     lateinit var maxTimeTextView: TextView
-    var currentPosition: Long = 0
+    lateinit var audioNameTextView: TextView
+    lateinit var playPauseButton: FloatingActionButton
+    lateinit var backButton: FloatingActionButton
+    lateinit var forwardButton: FloatingActionButton
+
+    lateinit var audioList: MutableList<Int>
+    var elapsedTime: Long = 0
+    var path: Int = 0
 
 
     override fun onCreateView(
@@ -37,95 +44,92 @@ class StudyWithAudioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-         binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.study_with_audio_fragment,
-            container,
-            false
-        )
+        binding = DataBindingUtil.inflate(inflater, R.layout.study_with_audio_fragment, container, false)
+
+        setUp()
+        initializeSeekBar()
+        setUpMediaPlayer()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+    private fun setUp(){
 
         handler = Handler()
         runnable = Runnable {}
-        mediaPlayer = MediaPlayer.create(context, R.raw.civicquestions)
 
         seekBar = binding.audioSeekbar
-        seekBar.max = mediaPlayer.duration
-
+        audioNameTextView = binding.audioNameTextview
         currentTimeTextView = binding.audioCurrentTimeTextView
         maxTimeTextView = binding.audioMaxTimeTextView
+        playPauseButton = binding.audioPlayPauseButton
+        backButton = binding.audioBackButton
+        forwardButton = binding.audioForwardButton
 
-        initializeSeekBar()
+        audioList = mutableListOf(R.raw.branches_of_government, R.raw.amount_of_senators, R.raw.delclaration_of_independence, R.raw.economic_system_of_us,
+            R.raw.first_ten_amendments_of_constitution, R.raw.first_three_words_of_constitution, R.raw.freedom_of_religion)
+
+        audioList.shuffle()
+
+        path = audioList[2]
+
+        mediaPlayer = MediaPlayer.create(context, path)
+        seekBar.max = mediaPlayer.duration
+
+        val nameOfAudio: String = resources.getResourceName(path)
+        audioNameTextView.text = resources.getResourceName(path).subSequence(41, nameOfAudio.length)
+    }
+
+    private fun setUpMediaPlayer() {
+
+        mediaPlayer.setOnCompletionListener {
+            mediaPlayer.pause()
+            playPauseButton.setImageResource(R.drawable.play_arrow)
+        }
 
         val isPlaying: Boolean = mediaPlayer.isPlaying
-        binding.audioPlayPauseButton.setImageResource(if (isPlaying) R.drawable.pause_icon else R.drawable.play_arrow)
+        playPauseButton.setImageResource(if (isPlaying) R.drawable.pause_icon else R.drawable.play_arrow)
 
-        val nameOfAudio: String = resources.getResourceName(R.raw.civicquestions)
-
-
-        binding.audioNameTextview.text = resources.getResourceName(R.raw.civicquestions).subSequence(41, nameOfAudio.length)
-
-        binding.audioPlayPauseButton.setOnClickListener {
+        playPauseButton.setOnClickListener {
             val isPlaying: Boolean = mediaPlayer.isPlaying
 
             mediaPlayer.start()
 
             if (isPlaying) {
 
-                binding.audioPlayPauseButton.setImageResource(R.drawable.play_arrow)
+                playPauseButton.setImageResource(R.drawable.play_arrow)
                 mediaPlayer.pause()
                 updateSeekBar()
 
             } else {
-                binding.audioPlayPauseButton.setImageResource(R.drawable.pause_icon)
+                playPauseButton.setImageResource(R.drawable.pause_icon)
                 mediaPlayer.start()
 
                 updateSeekBar()
             }
         }
 
-        binding.audioBackButton.setOnClickListener {
+        backButton.setOnClickListener {
 
             mediaPlayer.seekTo(mediaPlayer.currentPosition - 6000)
         }
 
-        binding.audioForwardButton.setOnClickListener {
+        forwardButton.setOnClickListener {
 
             mediaPlayer.seekTo(mediaPlayer.currentPosition + 6000)
         }
 
-        mediaPlayer.setOnCompletionListener {
-            mediaPlayer.pause()
-        }
-
-
-        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
-
-            override fun onProgressChanged(
-                seekBar: SeekBar, progress: Int, fromUser: Boolean
-            ) {
-
-                if (fromUser && mediaPlayer != null) {
-                    mediaPlayer.seekTo(progress)
-
-                }
-            }
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
-
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-
-            }
-
-        })
-        return binding.root
     }
 
     private fun initializeSeekBar() {
 
         val maxTime = mediaPlayer.duration
 
-        val currentTimeInMins = TimeUnit.MINUTES.toMinutes(currentPosition)
+        val currentTimeInMins = TimeUnit.MINUTES.toMinutes(elapsedTime)
         val maxTimeInMins = TimeUnit.MINUTES.toMinutes(maxTime.toLong())
 
         val ctMinutes = (currentTimeInMins / 1000 / 60)
@@ -139,14 +143,36 @@ class StudyWithAudioFragment : Fragment() {
 
         currentTimeTextView.text = currentTimeInMinutes
         maxTimeTextView.text = maxTimeInMinutes
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+
+            override fun onProgressChanged(
+                seekBar: SeekBar, progress: Int, fromUser: Boolean
+            ) {
+
+                if (fromUser && mediaPlayer != null) {
+                    mediaPlayer.seekTo(progress)
+
+                }
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+        })
     }
 
-    private fun updateSeekBar(){
+    private fun updateSeekBar() {
         seekBar.progress = mediaPlayer.currentPosition
 
-        currentPosition = mediaPlayer.currentPosition.toLong()
+        elapsedTime = mediaPlayer.currentPosition.toLong()
 
-        val currentPositionTimeInMins = TimeUnit.MINUTES.toMinutes(currentPosition)
+        val currentPositionTimeInMins = TimeUnit.MINUTES.toMinutes(elapsedTime)
         val minutes = (currentPositionTimeInMins / 1000 / 60)
         val seconds = (currentPositionTimeInMins / 1000) % 60
 
@@ -155,35 +181,40 @@ class StudyWithAudioFragment : Fragment() {
 
 
         val maxTime = mediaPlayer.duration
-        val timeLeft = maxTime - currentPosition
+        val timeRemaining = maxTime - elapsedTime
 
-        val maxTimeInMins = TimeUnit.MINUTES.toMinutes(timeLeft)
+        val maxTimeInMins = TimeUnit.MINUTES.toMinutes(timeRemaining)
 
         val maxTimeMinutes = (maxTimeInMins / 1000 / 60)
         val maxTimeSeconds = (maxTimeInMins / 1000) % 60
-        val maxTimeInMinutes = String.format(Locale.getDefault(), "%02d:%02d", maxTimeMinutes, maxTimeSeconds)
+        val timeRemainingInMinutes =
+            String.format(Locale.getDefault(), "%02d:%02d", maxTimeMinutes, maxTimeSeconds)
 
-        if(mediaPlayer.isPlaying){
+        if (mediaPlayer.isPlaying) {
 
             runnable = Runnable {
-                run{
+                run {
 
                     updateSeekBar()
                     currentTimeTextView.text = currentPositionInMinutes
-                    maxTimeTextView.text = maxTimeInMinutes
+                    maxTimeTextView.text = timeRemainingInMinutes
                 }
             }
             handler.postDelayed(runnable, 1000)
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
+    private fun stopAudio(){
         mediaPlayer.stop()
         mediaPlayer.reset()
         mediaPlayer.release()
 
         handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        stopAudio()
     }
 }
